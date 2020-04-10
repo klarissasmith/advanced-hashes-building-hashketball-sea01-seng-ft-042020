5f99c837d634f3e5303686cd2bfdfac391c2bdb9
Klarissa Smith <ksmith_email@yahoo.com>
2020-04-10 15:30:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,7 @@ game_hash =
 :home => {
   :team_name => "Brooklyn Nets",
   :colors => ["Black", "White"],
-  :players => [
+  :players => {
 
   "Alan Anderson" => {
     :number => 0,
@@ -57,7 +57,7 @@ game_hash =
     :blocks => 11,
     :slam_dunks => 1
 
-]
+}
 }
 },
 :away => {
